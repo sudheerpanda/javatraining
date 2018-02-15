e69dcd7c454bb1c38b76fa611fbf1e1de1c11718
sudheerpanda <sudheer.panda@imaginea.com>
2018-02-15 12:33:51 +0530

    added custom annotation

---
+++ annotations/src/main/java/com/imaginea/annotations/Validate.java
@@ -0,0 +1,16 @@
+package com.imaginea.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
+@interface Validate {
+    String message() default "Consistency check failed";
+
+    String match();
+
+}
+
